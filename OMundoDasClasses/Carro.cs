814c3611d5a21058de011a1ddaf396bb582865f2
Elton Guilherme <elton.guilherme@hotmail.com>
2022-08-10 02:22:16 -0300

    Aula 1 - Parte 1 das Classes

---
+++ OMundoDasClasses/Carro.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parte1
+{
+    public class Carro
+    {
+        //atributos do carro
+        public Cor Cor { get; set; }
+        public int Portas { get; }
+        public string Modelo { get; }
+
+        private bool ligado = false; //private só a classe tem acesso
+
+        public bool Ligado //Propriedade: define uma maneira de acessar atributos
+        {
+            get //retorna valor
+            {
+                return ligado;
+            }
+        }
+
+        //Método construtor da classe
+        public Carro(Cor cor, int portas, string modelo)
+        {
+            Cor = cor;
+            Portas = portas;
+            Modelo = modelo;
+        }
+
+        //Comportamentos do carro
+        public string Ligar()
+        {
+            ligado = true;
+            return "O carro foi ligado.";
+            
+        }
+        public string Desligar()
+        {
+            ligado = false;
+            return "O carro foi desligado";
+            
+        }
+
+        public string Andar()
+        {
+            return "O Carro está andando.";
+        }
+    }
+}
---
+++ OMundoDasClasses/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parte1
+{
+    class Program //Sem modificador de acesso explícito = internal por padrão
+    {
+        static void Main(string[] args)
+        {
+            Carro carro = new Carro(Cor.Branco, 4, "Gol G6"); //criando carro
+            Console.WriteLine(carro.Ligar()); //Ligar carro
+            Console.WriteLine("Ligado? " + carro.Ligado);
+            Console.WriteLine(carro.Andar()); //Andar com carro
+            Console.WriteLine(carro.Desligar()); //DesLigar carro
+            Console.WriteLine("Ligado? " + carro.Ligado);
+
+            //Exibindo atributos do carro
+            Console.WriteLine("O carro é um " + carro.Modelo + " de cor " + carro.Cor + " e tem " + carro.Portas + " portas.");
+
+            carro.Cor = Cor.Azul;
+            Console.WriteLine("A cor do carro agora é: " + carro.Cor);
+
+        }
+    }
+}
