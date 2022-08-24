1cc92f2a4515ab6cf8692fd021f046e0cf3d114a
Elton Guilherme <elton.guilherme@hotmail.com>
2022-08-24 01:30:10 -0300

    Aula 4 - Classes Pai e Filho

---
+++ Parte2/Gato.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parte2
+{
+    public class Gato : Animal
+    {
+        public Gato(string nome, int idade) : base(nome, idade) //base(nome,idade) invoca o construtor da classe Animal
+        {
+        }
+    }
+}
---
+++ Parte2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parte2
+{
+    class Program //Sem modificador de acesso explícito = internal por padrão
+    {
+        static void Main(string[] args)
+        {
+            var c = new Cachorro("Rex", 5);
+            var g = new Gato("Ana", 2);
+
+            Animal a = new Gato("Felix", 5); //Polimorfismo: O gato também é um animal!
+
+            ExibeAnimal(c); //recebe argumento do tipo cachorro
+            ExibeAnimal(g); //recebe argumento do tipo gato
+            ExibeAnimal(a); //recebe argumento do tipo animal
+        }
+
+        public static void ExibeAnimal(Animal animal)
+        {
+            Console.WriteLine($"Nome: {animal.Nome}");
+            Console.WriteLine($"Idade: {animal.Idade}");
+        }
+        
+    }
+
+}
