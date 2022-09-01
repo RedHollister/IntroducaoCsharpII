bd5b124ec3bf0379fd7845a6519d2dd5425fd6ba
Elton Guilherme <elton.guilherme@hotmail.com>
2022-09-01 02:12:28 -0300

    Aula 5 Você Disse Interfaces?

---
+++ VoceDisseInterfaces/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoceDisseInterfaces
+{
+    class Program //Sem modificador de acesso explícito = internal por padrão
+    {
+        static void Main(string[] args)
+        {
+            var i = new ImpressoraComum();
+            var i2 = new ImpressoraCopiadora();
+            
+            Console.WriteLine(i.Imprimir("Estou estudando C#"));
+            Console.WriteLine(i2.Copiar("Estou estudando C#"));
+
+            CopiarDocumento(new Xerox(), "Hello World!");
+        }
+
+        public static void CopiarDocumento(ICopiadora c, string texto)
+        {
+            Console.WriteLine("Estou copiando o texto a seguir: " + c.Copiar(texto));
+        }
+
+    }
+
+}
